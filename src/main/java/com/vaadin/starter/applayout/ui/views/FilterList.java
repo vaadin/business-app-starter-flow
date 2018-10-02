b0067b34f4774561bb3b563a0075b5114b959eb6
Joacim Päivärinne <joacim@vaadin.com>
2018-10-02 15:33:47 +0300

    Filter list 1.0 done

--- src/main/java/com/vaadin/starter/applayout/ui/views/FilterList.java
+++ src/main/java/com/vaadin/starter/applayout/ui/views/FilterList.java
@@ -24,7 +24,9 @@ import com.vaadin.starter.applayout.backend.DummyData;
 import com.vaadin.starter.applayout.backend.Person;
 import com.vaadin.starter.applayout.ui.Root;
 import com.vaadin.starter.applayout.ui.components.AbstractView;
+import com.vaadin.starter.applayout.ui.components.AppBar;
 import com.vaadin.starter.applayout.ui.components.ListItem;
+import com.vaadin.starter.applayout.ui.utils.BoxShadowBorders;
 import com.vaadin.starter.applayout.ui.utils.CSSProperties;
 import com.vaadin.starter.applayout.ui.utils.LumoStyles;
 import com.vaadin.starter.applayout.ui.utils.UIUtils;
@@ -39,18 +41,23 @@ import static com.vaadin.starter.applayout.ui.utils.ViewStyles.GRID_VIEW;
 @PageTitle("Filter list")
 public class FilterList extends AbstractView {
 
-    private String FILTER_SECTION = "filter-section";;
-
+    private AppBar appBar;
     private Grid<Person> grid;
 
     private Div filterArea;
     private Button toggleButton;
     private FlexLayout tokens;
     private FlexLayout options;
+    private FlexLayout filterHeader;
+    private FlexLayout content;
 
     public FilterList() {
+        // Header
+        appBar = new AppBar("Dashboard");
+
+        // Filters
         filterArea = UIUtils.createDiv(
-                Arrays.asList("filter-area", LumoStyles.Padding.Responsive.Horizontal.ML, LumoStyles.Shadow.M),
+                Arrays.asList("filter-area", LumoStyles.Padding.Responsive.Horizontal.SL),
                 createFilterHeader(),
                 UIUtils.createWrappingFlexLayout(
                         Collections.singleton(LumoStyles.Spacing.Right.L),
@@ -58,7 +65,6 @@ public class FilterList extends AbstractView {
                         createTokens()
                 )
         );
-        filterArea.getElement().setAttribute(LumoStyles.THEME, LumoStyles.DARK);
 
         // Grid
         grid = new Grid();
@@ -85,24 +91,27 @@ public class FilterList extends AbstractView {
                 .setSortable(true)
                 .setWidth("160px")
                 .setFlexGrow(0);
-        grid.setSizeFull();
 
         DataProvider dataProvider = DataProvider.ofCollection(DummyData.getPersons());
         grid.setDataProvider(dataProvider);
+
+        // Content wrapper
+        content = UIUtils.createColumn(Collections.singleton(FILTER_LIST_VIEW), filterArea, grid);
+        content.setHeight("100%");
+
     }
 
     @Override
     protected void initSlots() {
-        setHeader(filterArea);
-        setContent(grid);
-        getContent().addClassName(FILTER_LIST_VIEW);
+        setHeader(appBar);
+        setContent(content);
     }
 
     private FlexLayout createFilterHeader() {
         toggleButton = UIUtils.createTertiaryIconButton(VaadinIcon.CHEVRON_UP_SMALL);
         toggleButton.addClickListener(event -> toggleFilterArea());
 
-        Label title = UIUtils.createLabel(Collections.singleton(LumoStyles.FontSize.H4), "Filter");
+        Label title = UIUtils.createLabel(Collections.singleton(LumoStyles.FontSize.H5), "Filter");
 
         Label badge = new Label("4");
         badge.getElement().setAttribute(LumoStyles.THEME, LumoStyles.Badge.CONTRAST);
@@ -111,13 +120,13 @@ public class FilterList extends AbstractView {
         search.setPlaceholder("Quick filter...");
         search.addClassName(LumoStyles.Margin.Left.AUTO);
 
-        FlexLayout header = UIUtils.createFlexLayout(Arrays.asList(LumoStyles.Padding.Vertical.XS, LumoStyles.Spacing.Right.S), toggleButton, title, badge, search);
-        header.setAlignItems(FlexComponent.Alignment.CENTER);
-        return header;
+        filterHeader = UIUtils.createFlexLayout(Arrays.asList("filter-header", BoxShadowBorders.BOTTOM, LumoStyles.Padding.Vertical.XS, LumoStyles.Spacing.Right.S), toggleButton, title, badge, search);
+        filterHeader.setAlignItems(FlexComponent.Alignment.CENTER);
+        return filterHeader;
     }
 
     private FlexLayout createFilterOptions() {
-        Label title = UIUtils.createLabel(Arrays.asList(LumoStyles.FontSize.H5), "Options");
+        Label title = UIUtils.createLabel(Arrays.asList(LumoStyles.FontSize.H6), "Options");
 
         ComboBox combo = new ComboBox();
         combo.setPlaceholder("Select...");
@@ -127,12 +136,13 @@ public class FilterList extends AbstractView {
         RadioButtonGroup optionGroup = new RadioButtonGroup();
         optionGroup.setItems("Option 1", "Option 2", "Option 3");
 
-        options = UIUtils.createColumn(Arrays.asList(FILTER_SECTION, LumoStyles.Padding.Vertical.M, LumoStyles.Spacing.Bottom.S), title, combo, checkbox, optionGroup);
+        options = UIUtils.createColumn(Arrays.asList(LumoStyles.Padding.Vertical.M, LumoStyles.Spacing.Bottom.S), title, combo, checkbox, optionGroup);
+        options.getStyle().set(CSSProperties.FlexGrow.PROPERTY, CSSProperties.FlexGrow._1);
         return options;
     }
 
     private Component createTokens() {
-        Label title = UIUtils.createLabel(Arrays.asList(LumoStyles.FontSize.H5), "Tokens");
+        Label title = UIUtils.createLabel(Arrays.asList(LumoStyles.FontSize.H6), "Tokens");
 
         ComboBox combo = new ComboBox();
         combo.setClassName("token-select");
@@ -143,7 +153,8 @@ public class FilterList extends AbstractView {
 
         Div tokenArea = new Div(token);
 
-        tokens = UIUtils.createColumn(Arrays.asList(FILTER_SECTION, LumoStyles.Padding.Vertical.M, LumoStyles.Spacing.Bottom.S), title, combo, tokenArea);
+        tokens = UIUtils.createColumn(Arrays.asList(LumoStyles.Padding.Vertical.M, LumoStyles.Spacing.Bottom.S), title, combo, tokenArea);
+        tokens.getStyle().set(CSSProperties.FlexGrow.PROPERTY, CSSProperties.FlexGrow._1);
         return tokens;
     }
 
@@ -175,6 +186,12 @@ public class FilterList extends AbstractView {
         options.setVisible(!options.isVisible());
         tokens.setVisible(!tokens.isVisible());
         toggleButton.setIcon(options.isVisible() ? new Icon(VaadinIcon.CHEVRON_UP_SMALL) : new Icon(VaadinIcon.CHEVRON_DOWN_SMALL));
+
+        if (options.isVisible()) {
+            filterHeader.addClassName(BoxShadowBorders.BOTTOM);
+        } else {
+            filterHeader.removeClassName(BoxShadowBorders.BOTTOM);
+        }
     }
 
 }
