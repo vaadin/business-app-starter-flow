debaf36455dfa662d9c2c200f86e0c5137ab1c8b
Jens Jansson <peppe@vaadin.com>
2019-04-23 10:27:22 +0300

    fixes #4. - makes accounts columns resizable - makes accounts availability column smaller so that there isn't white space, while the owner column is ellipsed.

--- src/main/java/com/vaadin/starter/business/ui/views/Accounts.java
+++ src/main/java/com/vaadin/starter/business/ui/views/Accounts.java
@@ -49,19 +49,21 @@ public class Accounts extends ViewFrame implements RouterLayout {
 
         grid.addColumn(BankAccount::getId).setFlexGrow(0).setFrozen(true)
                 .setHeader("ID").setSortable(true)
-                .setWidth(UIUtils.COLUMN_WIDTH_XS);
+                .setWidth(UIUtils.COLUMN_WIDTH_XS).setResizable(true);
         grid.addColumn(new ComponentRenderer<>(this::createBankInfo))
-                .setHeader("Bank Account").setWidth(UIUtils.COLUMN_WIDTH_XL);
+                .setHeader("Bank Account").setWidth(UIUtils.COLUMN_WIDTH_XL)
+                .setResizable(true);
         grid.addColumn(BankAccount::getOwner).setHeader("Owner")
-                .setWidth(UIUtils.COLUMN_WIDTH_XL);
+                .setWidth(UIUtils.COLUMN_WIDTH_XL).setResizable(true);
         grid.addColumn(new ComponentRenderer<>(this::createAvailability))
                 .setFlexGrow(0).setHeader("Availability ($)")
-                .setWidth(UIUtils.COLUMN_WIDTH_M)
+                .setWidth("130px").setResizable(true)
                 .setTextAlign(ColumnTextAlign.END);
         grid.addColumn(new LocalDateRenderer<>(BankAccount::getUpdated,
                 DateTimeFormatter.ofPattern("MMM dd, YYYY")))
                 .setComparator(BankAccount::getUpdated).setFlexGrow(0)
-                .setHeader("Updated").setWidth(UIUtils.COLUMN_WIDTH_M);
+                .setHeader("Updated").setWidth(UIUtils.COLUMN_WIDTH_M)
+                .setResizable(true);
 
         return grid;
     }
