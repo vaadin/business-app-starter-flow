0fe1c0d039312d545dfe0597ade95e5a21e4761b
Jens Jansson <peppe@vaadin.com>
2019-07-30 10:28:01 +0300

    Fixes app bar styles in IE11

--- frontend/styles/components/app-bar.js
+++ frontend/styles/components/app-bar.js
@@ -19,6 +19,8 @@ style(`
   .app-bar__navi-icon,
   .app-bar__context-icon {
     margin-right: var(--lumo-space-l);
+    margin-bottom: calc((var(--app-bar-height-mobile) - var(--lumo-icon-size-m)) / 2);
+    margin-top: calc((var(--app-bar-height-mobile) - var(--lumo-icon-size-m)) / 2);
   }
 
   /* Title */
@@ -30,9 +32,18 @@ style(`
     white-space: nowrap;
   }
 
+  .app-bar__title:not(:empty) {
+    margin-bottom: calc((var(--app-bar-height-mobile) - (var(--lumo-font-size-l) * var(--lumo-line-height-xs))) / 2);
+    margin-top: calc((var(--app-bar-height-mobile) - (var(--lumo-font-size-l) * var(--lumo-line-height-xs))) / 2);
+  }
+
   /* Action items */
   .app-bar__action-items > *:not(:last-child) {
     margin-right: var(--lumo-space-s);
+  }
+
+  .app-bar__tab-container {
+    padding: 0 var(--lumo-space-m);
   }
 
   /* Search */
@@ -62,40 +73,39 @@ style(`
     flex-shrink: 0;
   }
 
-  @media (max-width: 1023px) {
-    .app-bar__navi-icon,
-    .app-bar__context-icon {
-      margin-bottom: calc(calc(var(--app-bar-height-mobile) - var(--lumo-icon-size-m)) / 2);
-      margin-top: calc(calc(var(--app-bar-height-mobile) - var(--lumo-icon-size-m)) / 2);
-    }
-
-    .app-bar__title:not(:empty) {
-      margin-bottom: calc(calc(var(--app-bar-height-mobile) - calc(var(--lumo-font-size-l) * var(--lumo-line-height-xs))) / 2);
-      margin-top: calc(calc(var(--app-bar-height-mobile) - calc(var(--lumo-font-size-l) * var(--lumo-line-height-xs))) / 2);
-    }
-
-    .app-bar__tab-container {
-      padding: 0 var(--lumo-space-m);
-    }
-  }
-
   @media (min-width: 1024px) {
     .app-bar__navi-icon {
       display: none;
     }
 
     .app-bar__context-icon {
-      margin-bottom: calc(calc(var(--app-bar-height-desktop) - var(--lumo-icon-size-m)) / 2);
-      margin-top: calc(calc(var(--app-bar-height-desktop) - var(--lumo-icon-size-m)) / 2);
+      margin-bottom: calc((var(--app-bar-height-desktop) - var(--lumo-icon-size-m)) / 2);
+      margin-top: calc((var(--app-bar-height-desktop) - var(--lumo-icon-size-m)) / 2);
     }
 
     .app-bar__title:not(:empty) {
-      margin-bottom: calc(calc(var(--app-bar-height-desktop) - calc(var(--lumo-font-size-l) * var(--lumo-line-height-xs))) / 2);
-      margin-top: calc(calc(var(--app-bar-height-desktop) - calc(var(--lumo-font-size-l) * var(--lumo-line-height-xs))) / 2);
+      margin-bottom: calc((var(--app-bar-height-desktop) - (var(--lumo-font-size-l) * var(--lumo-line-height-xs))) / 2);
+      margin-top: calc((var(--app-bar-height-desktop) - (var(--lumo-font-size-l) * var(--lumo-line-height-xs))) / 2);
     }
 
     .app-bar__tab-container {
       padding: 0 var(--lumo-space-l);
     }
   }
+
+    /* IE 11 workarounds */
+    @media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
+      /* IE 11 can't handle different themes for sub-parts of the application, without the parts having own
+         shadow roots. Instead of polluting the code for all browsers, the relevant colors of dark theme
+         is hard coded here. */
+      .app-bar {
+        color: hsl(214, 100%, 98%);
+        background-color: hsl(214, 35%, 21%);
+        box-shadow: 0 2px 4px -1px hsla(214, 8%, 4%, 0.23), 0 3px 12px -1px hsla(214, 12%, 6%, 0.32);
+      }
+
+      .app-bar__title {
+        color: hsl(214, 100%, 98%);
+      }
+    }
 `)
