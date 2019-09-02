bb20202144a18bfb6000e9778626a8c903a223cf
Joacim Päivärinne <joacim@vaadin.com>
2019-09-02 15:02:18 +0300

    Update Initials.java

--- src/main/java/com/vaadin/starter/business/ui/components/Initials.java
+++ src/main/java/com/vaadin/starter/business/ui/components/Initials.java
@@ -1,7 +1,6 @@
 package com.vaadin.starter.business.ui.components;
 
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.theme.lumo.Lumo;
 import com.vaadin.starter.business.ui.util.FontSize;
 import com.vaadin.starter.business.ui.util.FontWeight;
 import com.vaadin.starter.business.ui.util.LumoStyles;
@@ -14,13 +13,13 @@ public class Initials extends FlexBoxLayout {
 
     public Initials(String initials) {
         setAlignItems(FlexComponent.Alignment.CENTER);
+        setBackgroundColor(LumoStyles.Color.Contrast._10);
         setBorderRadius(BorderRadius.L);
         setClassName(CLASS_NAME);
         UIUtils.setFontSize(FontSize.S, this);
         UIUtils.setFontWeight(FontWeight._600, this);
         setHeight(LumoStyles.Size.M);
         setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
-        setTheme(Lumo.DARK);
         setWidth(LumoStyles.Size.M);
 
         add(initials);
