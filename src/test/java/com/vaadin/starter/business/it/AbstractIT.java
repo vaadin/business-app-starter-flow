77b5efd199338ebdd974f5f63e7330c0d1abb4d8
Anastasia Smirnova <anasmi@utu.fi>
2020-04-07 12:13:51 +0300

    Alter URL

--- src/test/java/com/vaadin/starter/business/it/AbstractIT.java
+++ src/test/java/com/vaadin/starter/business/it/AbstractIT.java
@@ -31,7 +31,7 @@ public abstract class AbstractIT extends ParallelTest {
     public void setup() throws Exception {
         super.setup();
         if (getRunLocallyBrowser() == null) {
-            APP_URL = getHubURL().replace("4445","7000");
+            APP_URL = getHubURL().replace("localhost","@ondemand.saucelabs.com");
         }
         System.out.println("SET-UP " + APP_URL);
     }
