b2db5a922944d228ec73dcb00eb1e22b2fb0ec35
Anastasia Smirnova <anasmi@utu.fi>
2020-04-08 13:56:27 +0300

    Clean-up code

--- src/test/java/com/vaadin/starter/business/it/AbstractIT.java
+++ src/test/java/com/vaadin/starter/business/it/AbstractIT.java
@@ -13,12 +13,6 @@ public abstract class AbstractIT extends ParallelTest {
     public ScreenshotOnFailureRule rule = new ScreenshotOnFailureRule(this,
             true);
 
-    //Uncomment this when running locally
-    //@Before
-   // public void startBrowser() {
-      //  setDriver(new ChromeDriver(new ChromeOptions().setHeadless(false)));
-    //}
-
     protected void assertNumbers(String expected, String actual) {
         // Remove any thousands and decimal separators before comparing
         Assert.assertEquals(stripSeparators(expected), stripSeparators(actual));
