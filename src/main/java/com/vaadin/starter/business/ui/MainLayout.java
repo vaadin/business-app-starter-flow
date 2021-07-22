70d9f87f0055a5fe2e1f4520491d298cef75e6ef
Manuel Carrasco Moñino <manolo@vaadin.com>
2021-07-22 10:12:43 +0200

    Fix favicon path (#256)
    * Fix favicon path
    
    Fixes: https://github.com/vaadin/platform/issues/2249
    
    * chore: remove favicon which is already covered by PWA annotation
    
    * chore: remove unused favicon images
    
    * fix: fix iconPath in pwa annotation

--- src/main/java/com/vaadin/starter/business/ui/MainLayout.java
+++ src/main/java/com/vaadin/starter/business/ui/MainLayout.java
@@ -44,7 +44,7 @@ import org.slf4j.LoggerFactory;
 @CssImport("./styles/misc/box-shadow-borders.css")
 @CssImport(value = "./styles/styles.css", include = "lumo-badge")
 @JsModule("@vaadin/vaadin-lumo-styles/badge")
-@PWA(name = "###Project Name###", shortName = "###Project Name###", iconPath = "images/logo-18.png", backgroundColor = "#233348", themeColor = "#233348")
+@PWA(name = "###Project Name###", shortName = "###Project Name###", iconPath = UIUtils.IMG_PATH + "logos/18.png", backgroundColor = "#233348", themeColor = "#233348")
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
 public class MainLayout extends FlexBoxLayout
 		implements RouterLayout, PageConfigurator, AfterNavigationObserver {
@@ -214,9 +214,6 @@ public class MainLayout extends FlexBoxLayout
 	public void configurePage(InitialPageSettings settings) {
 		settings.addMetaTag("apple-mobile-web-app-capable", "yes");
 		settings.addMetaTag("apple-mobile-web-app-status-bar-style", "black");
-
-		settings.addFavIcon("icon", "frontend/images/favicons/favicon.ico",
-				"256x256");
 	}
 
 	@Override
