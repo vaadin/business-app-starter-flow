5a0524997eef8d42d01736145566d3e5f2343529
Manuel Carrasco Moñino <manolo@vaadin.com>
2021-07-22 10:37:31 +0200

    fix: fix iconPath in pwa annotation (#257)
    * fix: fix iconPath in pwa annotation
    
    * chore: remove favicon since it is covered by PWA
    
    * chore: remove unused images

--- src/main/java/com/vaadin/starter/business/ui/AppShell.java
+++ src/main/java/com/vaadin/starter/business/ui/AppShell.java
@@ -4,8 +4,9 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.starter.business.ui.util.UIUtils;
 
-@PWA(name = "###Project Name###", shortName = "###Project Name###", iconPath = "images/logo-18.png", backgroundColor = "#233348", themeColor = "#233348")
+@PWA(name = "###Project Name###", shortName = "###Project Name###", iconPath = UIUtils.IMG_PATH + "logos/18.png", backgroundColor = "#233348", themeColor = "#233348")
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
 public class AppShell implements AppShellConfigurator {
 
@@ -13,7 +14,5 @@ public class AppShell implements AppShellConfigurator {
 	public void configurePage(AppShellSettings settings) {
 		settings.addMetaTag("apple-mobile-web-app-capable", "yes");
 		settings.addMetaTag("apple-mobile-web-app-status-bar-style", "black");
-		settings.addFavIcon("icon", "frontend/images/favicons/favicon.ico",
-				"256x256");
 	}
 }
