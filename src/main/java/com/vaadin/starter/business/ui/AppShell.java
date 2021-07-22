d3899132dc20e72167ce1c35232d05d80319685b
Manolo Carrasco <manolo@vaadin.com>
2021-07-22 10:18:18 +0200

    fix: fix iconPath in pwa annotation

--- src/main/java/com/vaadin/starter/business/ui/AppShell.java
+++ src/main/java/com/vaadin/starter/business/ui/AppShell.java
@@ -4,8 +4,9 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.server.AppShellSettings;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.starter.business.ui.util.UIUtils;
 
-@PWA(name = "###Project Name###", shortName = "###Project Name###", iconPath = "images/logo-18.png", backgroundColor = "#233348", themeColor = "#233348")
+@PWA(name = "###Project Name###", shortName = "###Project Name###", iconPath = UIUtils.IMG_PATH + "logos/18.png", backgroundColor = "#233348", themeColor = "#233348")
 @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
 public class AppShell implements AppShellConfigurator {
 
