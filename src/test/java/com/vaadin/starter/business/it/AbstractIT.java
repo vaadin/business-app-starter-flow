a486a27e4438cb28fb11b485199aebc70c8a465f
Zhe Sun <zhe@vaadin.com>
2022-06-09 13:48:17 +0300

    update abstractIT

--- src/test/java/com/vaadin/starter/business/it/AbstractIT.java
+++ src/test/java/com/vaadin/starter/business/it/AbstractIT.java
@@ -1,5 +1,6 @@
 package com.vaadin.starter.business.it;
 
+import com.vaadin.testbench.Parameters;
 import com.vaadin.testbench.ScreenshotOnFailureRule;
 import com.vaadin.testbench.TestBench;
 import com.vaadin.testbench.parallel.ParallelTest;
@@ -7,9 +8,17 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 import org.junit.Rule;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.net.NetworkInterface;
+import java.util.Optional;
 
 public abstract class AbstractIT extends ParallelTest {
 
@@ -68,7 +77,7 @@ public abstract class AbstractIT extends ParallelTest {
      * @return URL to route
      */
     private static String getURL(String route) {
-        return String.format("http://%s:%d/%s", getDeploymentHostname(),
+        return String.format("http://%s:%s/%s", getDeploymentHostname(),
                 SERVER_PORT, route);
     }
 
@@ -84,6 +93,55 @@ public abstract class AbstractIT extends ParallelTest {
     protected void assertNumbers(String expected, String actual) {
         // Remove any thousands and decimal separators before comparing
         Assert.assertEquals(stripSeparators(expected), stripSeparators(actual));
+    }
+
+    protected String getPort() {
+        return "8080";
+    }
+
+    protected String getBaseURL() {
+        return "http://" + getCurrentHostAddress() + ":" + getPort();
+    }
+
+    private static Optional<String> getHostAddress(
+            NetworkInterface nwInterface) {
+        Enumeration<InetAddress> addresses = nwInterface.getInetAddresses();
+        while (addresses.hasMoreElements()) {
+            InetAddress address = addresses.nextElement();
+            if (address.isLoopbackAddress()) {
+                continue;
+            }
+            if (address.isSiteLocalAddress()) {
+                return Optional.of(address.getHostAddress());
+            }
+        }
+        return Optional.empty();
+    }
+
+    protected String getCurrentHostAddress() {
+        if (getRunOnHub(getClass()) == null
+                && Parameters.getHubHostname() == null) {
+            return "localhost";
+        }
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface nwInterface = interfaces.nextElement();
+                if (!nwInterface.isUp() || nwInterface.isLoopback()
+                        || nwInterface.isVirtual()) {
+                    continue;
+                }
+                Optional<String> address = getHostAddress(nwInterface);
+                if (address.isPresent()) {
+                    return address.get();
+                }
+            }
+        } catch (SocketException e) {
+            throw new RuntimeException("Could not find the host name", e);
+        }
+        throw new RuntimeException(
+                "No compatible (10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16) ip address found.");
     }
 
     private String stripSeparators(String string) {
