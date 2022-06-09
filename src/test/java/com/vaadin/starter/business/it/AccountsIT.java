d4eddc49a8c30c3dbd8f2babd90507908a74b3fb
Zhe Sun <zhe@vaadin.com>
2022-06-09 13:48:17 +0300

    update abstractIT

--- src/test/java/com/vaadin/starter/business/it/AccountsIT.java
+++ src/test/java/com/vaadin/starter/business/it/AccountsIT.java
@@ -3,13 +3,20 @@ package com.vaadin.starter.business.it;
 import com.vaadin.flow.component.grid.testbench.GridElement;
 import com.vaadin.flow.component.grid.testbench.GridTHTDElement;
 import com.vaadin.testbench.TestBenchElement;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AccountsIT extends AbstractIT {
 
+    @Before
+    public void init() {
+        String url = getBaseURL().replace(super.getBaseURL(),
+                super.getBaseURL() + "/accounts");
+        getDriver().get(url);
+    }
+
     @Test
     public void accountDetailsCorrect() {
-        getDriver().get("http://localhost:8080/accounts");
         GridElement accountsGrid = $(GridElement.class).id("accounts");
 
         GridTHTDElement availability = accountsGrid.getCell(0, 3);
