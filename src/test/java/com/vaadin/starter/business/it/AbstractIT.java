a70aab1d9d3302924eb0938d8d3f45f6eca739ea
Zhe Sun <zhe@vaadin.com>
2022-06-09 13:18:18 +0300

    chore: make it compatible to run the test in TC

--- src/test/java/com/vaadin/starter/business/it/AbstractIT.java
+++ src/test/java/com/vaadin/starter/business/it/AbstractIT.java
@@ -1,16 +1,30 @@
 package com.vaadin.starter.business.it;
 
 import com.vaadin.testbench.ScreenshotOnFailureRule;
-import com.vaadin.testbench.TestBenchTestCase;
+import com.vaadin.testbench.TestBench;
+import com.vaadin.testbench.parallel.ParallelTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-public abstract class AbstractIT extends TestBenchTestCase {
+public abstract class AbstractIT extends ParallelTest {
+
+    private static final int SERVER_PORT = 8080;
+    private final String route;
+    private final By rootSelector;
+
+    public AbstractIT() {
+        this("", By.tagName("body"));
+    }
+
+    protected AbstractIT(String route, By rootSelector) {
+        this.route = route;
+        this.rootSelector = rootSelector;
+    }
 
     @Rule
     public ScreenshotOnFailureRule rule = new ScreenshotOnFailureRule(this,
@@ -22,8 +36,49 @@ public abstract class AbstractIT extends TestBenchTestCase {
     }
 
     @Before
-    public void startBrowser() {
-        setDriver(new ChromeDriver(new ChromeOptions().setHeadless(false)));
+    public void setup() throws Exception {
+        if (isUsingHub()) {
+            super.setup();
+        } else {
+            setDriver(TestBench.createDriver(new ChromeDriver()));
+        }
+        getDriver().get(getURL(route));
+    }
+
+    /**
+     * Returns whether we are using a test hub. This means that the starter
+     * is running tests in Vaadin's CI environment, and uses TestBench to
+     * connect to the testing hub.
+     *
+     * @return whether we are using a test hub
+     */
+    private static boolean isUsingHub() {
+        return Boolean.TRUE.toString().equals(
+                System.getProperty(USE_HUB_PROPERTY));
+    }
+
+    /**
+     * Property set to true when running on a test hub.
+     */
+    private static final String USE_HUB_PROPERTY = "test.use.hub";
+
+    /**
+     * Returns deployment host name concatenated with route.
+     *
+     * @return URL to route
+     */
+    private static String getURL(String route) {
+        return String.format("http://%s:%d/%s", getDeploymentHostname(),
+                SERVER_PORT, route);
+    }
+
+    /**
+     * If running on CI, get the host name from environment variable HOSTNAME
+     *
+     * @return the host name
+     */
+    private static String getDeploymentHostname() {
+        return isUsingHub() ? System.getenv("HOSTNAME") : "localhost";
     }
 
     protected void assertNumbers(String expected, String actual) {
